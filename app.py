726e7d13e0d6432e5a1661667221c40bd97d9f9d
Sahmed29 <sahmed29@terpmail.umd.edu>
2026-02-18 12:50:43 -0500

    Add about.html page and update app.py with /about route

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "Hello World from Sharrima Ahmed! I am adding my first code change."
 
 @app.route('/hello')
-def hello_world():  # put application's code here
+def hello_world():
     return render_template('hello.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
