23d1e61295c12d3994952152e26e64c75c792574
Sahmed29 <sahmed29@terpmail.umd.edu>
2026-03-03 23:39:27 -0500

    Add about-css page and external stylesheet

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def hello_world():
 def about():
     return render_template('about.html')
 
+@app.route('/about-css')
+def about_css():
+    return render_template('about-css.html')
+
 if __name__ == '__main__':
     app.run()
+
